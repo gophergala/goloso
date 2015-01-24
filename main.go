2885de48ba203132c8b73e61d432ad312c5da886
include <francisco.cabrita@gmail.com>
2015-01-24 13:33:22 +0000

    flag options regarding channel name and topic

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	showHelp = flag.Bool("help", false, "print help")
+	topic    = flag.String("topic", "", "NSQ topic")
+	channel  = flag.String("channel", "", "NSQ channel")
 )
 
 func main() {
 	fmt.Println("Goloso")
+
+	flag.Parse()
+
+	if *showHelp {
+		fmt.Println(`
+Usage:
+    goloso --help
+
+    goloso --channel "orc.sys.events" --topic "ec2"
+`)
+		os.Exit(0)
+	}
+
+	if *channel == "" {
+		log.Fatalln("Err: missing channel")
+		os.Exit(1)
+	}
+
+	if *topic == "" {
+		log.Fatalln("Err: missing topic")
+		os.Exit(1)
+	}
+
 }
